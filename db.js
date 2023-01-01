eba87e32d2a5de2a40650db3df47a3d01ae267d9
sudhakar1504 <sudhakarkumar1504@gmail.com>
2023-01-01 20:40:40 +0530

    db change and url 4

--- db.js
+++ db.js
@@ -4,5 +4,12 @@ export const db = mysql.createConnection({
     host:"sql6.freesqldatabase.com",
     user:'sql6587120',
     password:'Gwh5QVbHm5',
-    database:"sql6587120"
+    database:"sql6587120",
+
+    connectionLimit : 100,
+        waitForConnections : true,
+        queueLimit :0,
+        debug    :  true,
+        wait_timeout : 28800,
+        connect_timeout :10
 })
